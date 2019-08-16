b40ba35d448130eb598bc132a689a86185cbaffc
Graeme T. Lloyd <graemetlloyd@gmail.com>
2019-08-16 13:30:40 +0100

    Adding new strap-package file.

---
+++ R/strap-package.R
@@ -0,0 +1,85 @@
+# devtools::build_win(version = "R-devel")
+
+#' Measuring Morphological Diversity and Evolutionary Tempo
+#' 
+#' A series of functions for stratigraphic analysis of phylogenetic trees.
+#' 
+#' @name strap-package
+#'
+#' @aliases strap
+#'
+#' @docType package
+#'
+#' @author Mark A. Bell <mark.bell521@@gmail.com>>
+#'
+#' @references
+#'
+#' Bell, M. A. and Lloyd, G. T., 2015. strap: an R package for plotting phylogenies against stratigraphy and assessing their stratigraphic congruence. Palaeontology, 58, 379-389.
+#'
+#' @keywords congruence,geology,phylogeny,stratigraphy,timescale
+#'
+#' @examples
+#' 
+#' # Calculate stratigraphic fit measures treating ages as ranges:
+#' fit.to.strat.1 <- StratPhyloCongruence(trees = Dipnoi$tree,
+#'   ages = Dipnoi$ages, rlen = 0, method = "basic", samp.perm=5,
+#'   rand.perm = 5, hard = TRUE, randomly.sample.ages = FALSE,
+#'   fix.topology = FALSE, fix.outgroup = TRUE)#'
+#'
+#' # Show just the output for the input tree(s)
+#' fit.to.strat.1$input.tree.results
+#'
+#' @exportPattern "^[[:alpha:]]+"
+#' @import ape
+#' @import geoscale
+
+#' @importFrom stats runif
+NULL
+
+#' Phylogeny and age data for the Asaphidae
+#'
+#' Phylogeny (162 most parsimonious trees) and age data for Asaphidae genera (Trilobita, Asaphida) taken from Bell and Braddy (2012).
+#'
+#'
+#' @name Asaphidae
+#' @docType data
+#' @format A list containing 162 trees ($tree) and a matrix of first and last appearances ($ages).
+#' @references Bell, M. A. and Braddy, S. J., 2012, Cope’s rule in the Ordovician trilobite Family Asaphidae (Order Asaphida): patterns across multiple most parsimonious trees: Historical Biology, 24, 223-230.
+#' @keywords datasets
+NULL
+
+
+
+
+
+
+#' Phylogeny and age data for dipnoans (lungfish)
+#'
+#' Phylogeny (first most parsimonious tree) and age data for lungfish (Osteichthyes, Sarcopterygii, Dipnoi) taken from Lloyd et al. (2012).
+#'
+#'
+#' @name Dipnoi
+#' @docType data
+#' @format A list containing a tree ($tree) and a matrix of first and last appearances ($ages).
+#' @references Lloyd, G. T., Wang, S. C. and Brusatte, S. L., 2012. Identifying heterogeneity in rates of morphological evolution: discrete character change in the evolution of lungfish (Sarcopterygii; Dipnoi). Evolution, 66, 330-348.
+#' @keywords datasets
+NULL
+
+
+
+
+
+#' British regional stages for the Ordovician
+#' 
+#' The stratigraphic ranges for the British stages of the Ordovician.
+#' 
+#' 
+#' @name UKzones
+#' @docType data
+#' @format A matrix containing the start and end ages for the British stage subdivisions of the Ordovician.
+#' @references Webby, B.D., Paris, F., Droser, M.L., and Percival, I.G. (editors), 2004. The Great Ordovician Biodiversity Event. Columbia University Press, New York, 496 pp.
+#' @keywords datasets
+NULL
+
+
+
